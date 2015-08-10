b8d9164a7c255dcfa5d2d4d50b56ce41b1d6656c
Dirk Schöttle <dschoettle@web.de>
2015-08-11 01:38:34 +0200

    The buttons work for the first time

--- app.js
+++ app.js
@@ -3,12 +3,33 @@
 
   app.controller('ExpressionController', function($scope, $http){
     
-    $http.get('expressions.json').success(function(data) {
+    $http.get('https://raw.githubusercontent.com/der-dirk/Artikler2/master/expressions.json').success(function(data) {
       $scope.expressions = data;
-    });
       
-    //$scope.products=gems;
+      for (var key1 in $scope.expressions)
+      {
+        if ($scope.expressions.hasOwnProperty(key1))
+        {
+          for (var key2 in $scope.expressions[key1].matches)
+          {
+            if ($scope.expressions[key1].matches.hasOwnProperty(key2))
+            {
+              $scope.expressions[key1].candidates.push($scope.expressions[key1].matches[key2]);
+            }
+          }
+        }
+      }
+
+      $scope.clickMe = function(buttonData)
+      {
+        if (buttonData[0].indexOf(buttonData[1]) == -1)
+          $scope.ok = false;
+        else
+          $scope.ok = true;
+      };
+
+    });
   });
-  
+
 })();
 
